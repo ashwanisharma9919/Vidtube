387118dbe085c9dc79810e4b7aa35d973059f0d6
ashwanisharma9919 <109890808+ashwanisharma9919@users.noreply.github.com>
2024-11-06 08:19:48 +0530

    "name"

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -21,7 +21,7 @@ const Feed = () => {
       <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
         <SideBar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#ffffff' }}>
-          Copyright 2022 Utkarsh Pathrabe
+          Copyright 2023 Ashwani Kumar Sharma
         </Typography>
       </Box>
       <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
